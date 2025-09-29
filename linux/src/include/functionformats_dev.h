90ca3e7e6329bccbd67b4a0e92bfca6c33c83e16
HitotsumeGod <189803705+HitotsumeGod@users.noreply.github.com>
2025-09-29 12:53:18 -0400

    Create functionformats_dev.h

---
+++ linux/src/include/functionformats_dev.h
@@ -0,0 +1,26 @@
+/**
+ * jet_inject family
+ * 
+ * formula : jet_inject_x_(optional)y_(optional)z
+ * where x is an identification
+ * where y is a delivery
+ * where z is a execution
+ */
+jet_inject_pid();
+jet_inject_name();
+jet_inject_priv();
+jet_inject_pid_trace();
+jet_inject_name_trace();
+jet_inject_priv_trace();
+jet_inject_pid_offset();
+jet_inject_pid_smash();
+jet_inject_name_offset();
+jet_inject_name_smash();
+jet_inject_priv_offset();
+jet_inject_priv_smash();
+jet_inject_pid_trace_offset();
+jet_inject_pid_trace_smash()
+jet_inject_name_trace_offset()
+jet_inject_name_trace_smash()
+jet_inject_priv_trace_offset()
+jet_inject_priv_trace_smash()
